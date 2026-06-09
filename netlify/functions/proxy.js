be477ba2de444a5263fedfa0ebeb7642dfa7df3e
seunghyuck-heo <heogoon@gmail.com>
2026-06-09 22:16:40 +0900

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,28 +1,22 @@
-const http = require('http');
-
 exports.handler = async function(event) {
   const path = event.path.replace('/.netlify/functions/proxy', '');
   const url = 'http://openapi.seoul.go.kr:8088' + path;
-
-  return new Promise((resolve) => {
-    http.get(url, (res) => {
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        resolve({
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json'
-          },
-          body: data
-        });
-      });
-    }).on('error', (e) => {
-      resolve({
-        statusCode: 500,
-        body: JSON.stringify({ error: e.message })
-      });
-    });
-  });
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: data
+    };
+  } catch(e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message })
+    };
+  }
 };
